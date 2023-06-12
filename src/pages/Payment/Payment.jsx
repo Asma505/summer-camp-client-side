d8af8a4dce7659404ca6c54150df8678628b3be7
Asma505 <sonali.hcn@gmail.com>
2023-06-13 00:09:19 +0600

    payment method added

--- src/pages/Payment/Payment.jsx
+++ src/pages/Payment/Payment.jsx
@@ -1,11 +1,19 @@
+import { loadStripe } from "@stripe/stripe-js";
+import CheckoutForm from "./CheckoutForm";
+import { Elements } from "@stripe/react-stripe-js";
+import useCourse from "../../hooks/useCourse";
 
-
-
-const Payment = () => {  
-    
+const stripePromise = loadStripe(import.meta.env.VITE_PAYMENT_GATEWAY_PK);
+const Payment = () => {
+    const [course] = useCourse();
+    const total = course.reduce((sum, c)=> sum + c.price, 0);
+    const price = parseFloat(total.toFixed(2));
     return (
-        <div>
-            <h3>payment</h3>
+        <div className="w-8/12">
+            <h3>Please Process Payment</h3>
+            <Elements stripe={stripePromise}>
+                <CheckoutForm price={price}></CheckoutForm>
+            </Elements>
         </div>
     );
 };
